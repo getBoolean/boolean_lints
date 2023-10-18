c323cada31aa14b5f96295f0e5a55d5af461c168
getBoolean <danielwnewville@gmail.com>
2023-10-18 13:14:27 -0500

    Add expect_lint comments to example

--- example/lib/banned_code.dart
+++ example/lib/banned_code.dart
@@ -2,12 +2,17 @@ import 'package:example/hardcoded_strings.dart';
 
 void testingBannedCodeLint() {
   final bannedCodeUsage = BannedCodeUsage();
+  // expect_lint: banned_code
   BannedCodeUsage.test2();
+  // expect_lint: banned_code
   final res = BannedCodeUsage.test2();
   print(res);
+  // expect_lint: banned_code
   bannedCodeUsage.test();
 
+  // expect_lint: banned_code
   final bannedCodeUsage2 = BannedCodeUsage.test3();
+  // expect_lint: banned_code
   bannedCodeUsage2.test();
 }
 
