70bb1707b1655b452ce2ce678eaad842d1b7b285
getBoolean <danielwnewville@gmail.com>
2023-10-18 18:17:37 -0500

    Disabled AvoidHardcodedStringsRule

--- lib/boolean_lints.dart
+++ lib/boolean_lints.dart
@@ -1,4 +1,3 @@
-import 'package:boolean_lints/src/lints/avoid_hardcoded_strings.dart';
 import 'package:boolean_lints/src/lints/banned_code.dart';
 import 'package:custom_lint_builder/custom_lint_builder.dart';
 
@@ -7,7 +6,6 @@ PluginBase createPlugin() => _CustomLintTemplatePlugin();
 class _CustomLintTemplatePlugin extends PluginBase {
   @override
   List<LintRule> getLintRules(CustomLintConfigs configs) => [
-        AvoidHardcodedStringsRule(),
         BannedCodeRule(),
       ];
 
