7fbc393fbfb4ba9e59dfd453878bb15e1c1c50a7
getBoolean <danielwnewville@gmail.com>
2023-10-18 20:58:31 -0500

    Implement single banned id and banned class

--- lib/src/lints/banned_code.dart
+++ lib/src/lints/banned_code.dart
@@ -27,21 +27,25 @@ class BannedCodeRule extends OptionsLintRule {
       return;
     }
 
+    final linter = BannedCodeLinter(
+      resolver: resolver,
+      reporter: reporter,
+      context: context,
+      options: options,
+    );
+
     // TODO: Check id works for fields/getters/global vars/global methods
 
-    // TODO: only id
-    // TODO: only className
-    // TODO: only package
-    // TODO: only className and package
-    // TODO: only id and package
-    // only id and className
-    // id and className and package
-    final BannedCodeOption(:entries, :severity) = options.rules.bannedCode;
-    final lintCode = code.copyWith(errorSeverity: severity);
+    final BannedCodeOption(
+      :entries,
+      severity: ruleSeverity,
+    ) = options.rules.bannedCode;
+
     for (final entry in entries) {
-      final EntryOption(:id, :className, :package, :severity, :reason) = entry;
-      final entryCode =
-          lintCode.copyWith(errorSeverity: severity, problemMessage: reason);
+      final entryCode = code.copyWith(
+        errorSeverity: entry.severity ?? ruleSeverity ?? code.errorSeverity,
+        problemMessage: entry.reason,
+      );
 
       switch (entry) {
         // all non null
@@ -60,9 +64,9 @@ class BannedCodeRule extends OptionsLintRule {
         case EntryOption(:final package?):
           break;
         case EntryOption(:final className?):
-          break;
+          linter.banClassName(entryCode, className);
         case EntryOption(:final id?):
-          break;
+          linter.banId(entryCode, id);
 
         // all null
         case EntryOption():
@@ -70,63 +74,63 @@ class BannedCodeRule extends OptionsLintRule {
       }
 
       // lint the node if the id matches for method calls
-      context.registry.addSimpleIdentifier((node) {
-        final name = node.name;
-        if (name != id) {
-          return;
-        }
+      // context.registry.addSimpleIdentifier((node) {
+      //   final name = node.name;
+      //   if (name != id) {
+      //     return;
+      //   }
 
-        if (className != null) {
-          final parent = node.parent;
-          final parentEntity =
-              parent!.childEntities.firstWhere((element) => element != node);
-          if (parentEntity is SimpleIdentifier) {
-            final parentType = parentEntity.staticType;
-            final parentElement = parentEntity.staticElement;
-            final parentSource = parentElement?.librarySource;
-            if (package != null) {
-              final parentSourceName = parentSource?.uri.toString();
-              if (!matchesPackage(parentSourceName, package)) {
-                return;
-              }
-            }
+      //   if (className != null) {
+      //     final parent = node.parent;
+      //     final parentEntity =
+      //         parent!.childEntities.firstWhere((element) => element != node);
+      //     if (parentEntity is SimpleIdentifier) {
+      //       final parentType = parentEntity.staticType;
+      //       final parentElement = parentEntity.staticElement;
+      //       final parentSource = parentElement?.librarySource;
+      //       if (package != null) {
+      //         final parentSourceName = parentSource?.uri.toString();
+      //         if (!matchesPackage(parentSourceName, package)) {
+      //           return;
+      //         }
+      //       }
 
-            if (parentType != null) {
-              final parentTypeName =
-                  parentType.getDisplayString(withNullability: false);
-              if (parentTypeName != className) {
-                return;
-              }
-            } else if (parentElement != null) {
-              final parentElementName = parentElement.name;
-              if (parentElementName != className) {
-                return;
-              }
-            } else {
-              return;
-            }
-          } else if (parentEntity is NamedType) {
-            final parentTypeName = parentEntity.name2.lexeme;
-            final parentSource = parentEntity.element?.librarySource;
-            if (package != null) {
-              final parentSourceName = parentSource?.uri.toString();
-              if (!matchesPackage(parentSourceName, package)) {
-                return;
-              }
-            }
+      //       if (parentType != null) {
+      //         final parentTypeName =
+      //             parentType.getDisplayString(withNullability: false);
+      //         if (parentTypeName != className) {
+      //           return;
+      //         }
+      //       } else if (parentElement != null) {
+      //         final parentElementName = parentElement.name;
+      //         if (parentElementName != className) {
+      //           return;
+      //         }
+      //       } else {
+      //         return;
+      //       }
+      //     } else if (parentEntity is NamedType) {
+      //       final parentTypeName = parentEntity.name2.lexeme;
+      //       final parentSource = parentEntity.element?.librarySource;
+      //       if (package != null) {
+      //         final parentSourceName = parentSource?.uri.toString();
+      //         if (!matchesPackage(parentSourceName, package)) {
+      //           return;
+      //         }
+      //       }
 
-            if (parentTypeName != className) {
-              return;
-            }
-          } else {
-            return;
-          }
-        }
+      //       if (parentTypeName != className) {
+      //         return;
+      //       }
+      //     } else {
+      //       return;
+      //     }
+      //   }
 
-        // check if the package name matches the node's source file package
+      //   // check if the package name matches the node's source file package
 
-        reporter.reportErrorForNode(entryCode, node);
-      });
+      //   reporter.reportErrorForNode(entryCode, node);
+      // });
     }
   }
 
@@ -136,4 +140,79 @@ class BannedCodeRule extends OptionsLintRule {
 
   @override
   List<Fix> getFixes() => [];
+}
+
+class BannedCodeLinter {
+  const BannedCodeLinter({
+    required this.resolver,
+    required this.reporter,
+    required this.context,
+    required this.options,
+  });
+
+  final CustomLintResolver resolver;
+  final ErrorReporter reporter;
+  final CustomLintContext context;
+  final Options options;
+
+  void banId(
+    LintCode code,
+    String id,
+  ) {
+    context.registry.addSimpleIdentifier((node) {
+      final name = node.name;
+      if (name != id) {
+        return;
+      }
+
+      reporter.reportErrorForNode(code, node);
+    });
+
+    context.registry.addDeclaredIdentifier((node) {
+      final name = node.name.lexeme;
+      if (name != id) {
+        return;
+      }
+
+      reporter.reportErrorForNode(code, node);
+    });
+  }
+
+  void banClassName(
+    LintCode code,
+    String className,
+  ) {
+    context.registry.addSimpleIdentifier((node) {
+      switch (node) {
+        case SimpleIdentifier(:final staticType?):
+          final nodeTypeName =
+              staticType.getDisplayString(withNullability: false);
+          if (nodeTypeName != className) {
+            return;
+          }
+        case SimpleIdentifier(:final staticElement?):
+          final nodeElementName = staticElement.name;
+          if (nodeElementName != className) {
+            return;
+          }
+
+          if (node.parent is ConstructorDeclaration) {
+            return;
+          }
+        default:
+          return;
+      }
+
+      reporter.reportErrorForNode(code, node);
+    });
+
+    context.registry.addNamedType((node) {
+      final parentTypeName = node.name2.lexeme;
+      if (parentTypeName != className) {
+        return;
+      }
+
+      reporter.reportErrorForNode(code, node);
+    });
+  }
 }
