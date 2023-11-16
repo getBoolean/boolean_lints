c27dca687c42b1f36779c84abbbf4e17567811fe
getBoolean <getboolean@gmail.com>
2023-11-16 01:57:50 -0600

    Add API docs

--- lib/boolean_lints.dart
+++ lib/boolean_lints.dart
@@ -1,6 +1,10 @@
+/// Contains the entrypoint of the plugin for use by [custom_lint](https://pub.dev/packages/custom_lint).
+library;
+
 import 'package:boolean_lints/src/lints/banned_code.dart';
 import 'package:custom_lint_builder/custom_lint_builder.dart';
 
+/// This is the entry point of the plugin.
 PluginBase createPlugin() => _CustomLintTemplatePlugin();
 
 class _CustomLintTemplatePlugin extends PluginBase {
